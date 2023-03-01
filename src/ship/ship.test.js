ada249463f7bd491fa4f9d66d1df7e88eb33288b
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-01 08:15:25 +0100

    Add test for times ship has been hit

--- src/ship/ship.test.js
+++ src/ship/ship.test.js
@@ -9,3 +9,8 @@ test('can create ships with speciefied length', () => {
     const ship = ShipFactory(2);
     expect(ship.length).toBe(2);
 })
+
+test('returns number of times ship has been hit', () => {
+    const ship = ShipFactory();
+    expect(ship.timesHit).toBe(0);
+})
