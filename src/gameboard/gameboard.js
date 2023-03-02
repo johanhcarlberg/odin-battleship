3644f60412fd7fae60ca4ee114bdc1c34892d43a
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-02 08:29:26 +0100

    Add tests and functionality for receiving attacks

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -1,11 +1,17 @@
+import { ShipFactory } from "../ship/ship";
+
 export const GameboardFactory = (boardLength = 10) => {
     const length = boardLength;
+    const missedShots = [];
+    const getMissedShots = ()  => missedShots;
+
     const createBoard = () => {
         let totalLength = length * length;
         return new Array(totalLength);
     }
     let board = createBoard()
     const getBoard = () => board;
+    const _setBoard = (newBoard) => board = newBoard;
 
     const _getIndexFromCoord = (x,y) => {
         return (length * y) + x
@@ -14,8 +20,41 @@ export const GameboardFactory = (boardLength = 10) => {
         const index = _getIndexFromCoord(x, y);
         return index;
     }
+
+    const _setPos = (x, y, data) => {
+        const index = getPos(x, y);
+        index = data;
+    }
+
+    const receiveAttack = (x, y) => {
+        const ship = board[getPos(x,y)];
+        if (ship && ship.hit) {
+            ship.hit();
+        } else {
+            missedShots.push([x, y]);
+        }
+    }
+
+    
+
+    const placeShip = (size, pos) => {
+        const x = pos.x;
+        const y = pos.y;
+        const startIndex = getPos(x, y);
+        let ship = ShipFactory(size);
+        let newBoard = getBoard();
+        for(let i = 0; i < ship.getLength(); i++) {
+            newBoard[startIndex + i] = ship;
+        }
+
+        _setBoard(newBoard);
+        return board;
+    }
     return {
         getBoard,
         getPos,
+        placeShip,
+        receiveAttack,
+        getMissedShots
     }
 }
--- src/gameboard/gameboard.test.js
+++ src/gameboard/gameboard.test.js
@@ -19,3 +19,31 @@ test('can get array index from x and y coordinates', () => {
     expect(gameBoard.getPos(0, 2)).toBe(20);
     expect(gameBoard.getPos(5, 1)).toBe(15);
 })
+
+test('can place ships', () => {
+    let gameBoard = GameboardFactory(10);
+    expect(gameBoard[0]).toBeUndefined();
+    let newBoard = gameBoard.placeShip(2, {x: 0, y: 0})
+    expect(newBoard[0]).toBeDefined();
+    expect(newBoard[1]).toBeDefined();
+    expect(newBoard[1].getLength()).toBe(2);
+})
+
+test('can receive attacks', () => {
+    let gameBoard = GameboardFactory(10);
+    gameBoard.receiveAttack(0, 0);
+    let missedShots = gameBoard.getMissedShots();
+    expect(missedShots[0]).toEqual([0,0]);
+    gameBoard.receiveAttack(1,1);
+    missedShots = gameBoard.getMissedShots();
+    expect(missedShots[1]).toEqual([1,1]);
+})
+
+test('ship receives attack', () => {
+    let gameBoard = GameboardFactory(10);
+    gameBoard.placeShip(2, {x: 0, y: 0});
+    gameBoard.receiveAttack(0, 0);
+    let pos = gameBoard.getPos(0, 0);
+    const ship = gameBoard.getBoard()[pos];
+    expect(ship.getTimesHit()).toBe(1);
+})
