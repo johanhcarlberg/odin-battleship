5fe8fdfe500f0e963d865810da018a554f687a11
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-02 10:14:48 +0100

    Add basic AI player

--- src/player/player.js
+++ src/player/player.js
@@ -1,8 +1,29 @@
 import { GameboardFactory } from "../gameboard/gameboard";
+import { pipe } from "rambda";
 
 export const Player = () => {
-    let gameBoard = GameboardFactory();
+    let gameBoard = GameboardFactory()
     return {
         gameBoard,
     };
 }
+
+const isAIPlayer = () => {
+    const getNextAttack = () => {
+        let x = Math.floor(Math.random() * (Math.pow(10, 2)))
+        let y = Math.floor(Math.random() * (Math.pow(10, 2)))
+        return {
+            x,
+            y,
+        }
+    }
+
+    return {
+        getNextAttack
+    }
+}
+
+export const AIPlayer = pipe(
+    Player,
+    isAIPlayer
+);
--- src/player/player.test.js
+++ src/player/player.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@jest/globals';
-import { Player } from './player';
+import { Player, AIPlayer } from './player';
 
 test('can create player', () => {
     const player = Player();
@@ -10,4 +10,14 @@ test('player has gameboard', () => {
     const player = Player();
     expect(player.gameBoard).toBeDefined();
     expect(player.gameBoard.getBoard().length).toBe(100);
+})
+
+test('AI player can generate attacks', () => {
+    const aiPlayer = AIPlayer();
+    expect(aiPlayer.getNextAttack).toBeDefined();
+    let firstAttack = aiPlayer.getNextAttack();
+    let secondAttack = aiPlayer.getNextAttack();
+    expect(firstAttack).not.toEqual(secondAttack);
+    expect(firstAttack.x).toBeGreaterThan(0);
+    expect(firstAttack.x).toBeLessThan(100);
 })
