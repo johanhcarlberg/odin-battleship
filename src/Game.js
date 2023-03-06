e2dda654c11d10951f62f210261e23e2245e96c3
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-06 14:25:07 +0100

    Change game to es6 class

--- src/Game.js
+++ src/Game.js
@@ -2,20 +2,14 @@ import { Player, AIPlayer } from "./player/player";
 import { GameboardComponent } from "./gameboard/GameboardComponent";
 
 
-export const Game = () => {
-    const player1 = Player();
-    player1.gameBoard.generateBoard();
-    
-    const player2 = AIPlayer();
-    player2.gameBoard.generateBoard();
+export class Game {
+    constructor() {
+        this.player1 = Player();
+        this.player1.gameBoard.generateBoard();
+        this.player2 = AIPlayer();
+        this.player2.gameBoard.generateBoard();
 
-    const p1gameBoard = new GameboardComponent(player1);
-    const p2gameBoard = new GameboardComponent(player2, true);
-
-    return {
-        player1,
-        player2,
-        p1gameBoard,
-        p2gameBoard
-    }
+        this.player1gameBoard = new GameboardComponent(this.player1, false);
+        this.player2gameBoard = new GameboardComponent(this.player2, true);
+        }
 }
--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ import { Game } from "./Game";
     console.log('battleship app loaded');
     const wrapper = document.querySelector('.wrapper');
     
-    const gameObj = Game();
-    console.log(gameObj);
-    wrapper.appendChild(gameObj.p1gameBoard.render());
-    wrapper.appendChild(gameObj.p2gameBoard.render());
+    const game = new Game();
+    wrapper.appendChild(game.player1gameBoard.render());
+    wrapper.appendChild(game.player2gameBoard.render());
 })();
