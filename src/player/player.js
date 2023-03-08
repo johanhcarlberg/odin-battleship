8ab2979e579ca86e62cfb53184ba817f60d9acef
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-08 15:18:14 +0100

    Add functionality for getting AI players performed attacks, check for performed attacks when getting next attacks

--- src/player/player.js
+++ src/player/player.js
@@ -16,15 +16,31 @@ export const Player = () => {
 }
 
 const isAIPlayer = (player = {}) => {
+    const performedAttacks = [];
+    const getPerformedAttacks = () => performedAttacks;
+
+    const hasPerformedAttack = (pos) => {
+        const posString = JSON.stringify(pos);
+        return performedAttacks.some((item) => {
+            return JSON.stringify(item) === posString;
+        })
+    }
+
     const getNextAttack = () => {
         let x = Math.floor(Math.random() * player.gameBoard.getLength())
         let y = Math.floor(Math.random() * player.gameBoard.getLength())
+        
+        if (hasPerformedAttack([x,y])) {
+            return getNextAttack();
+        }
+        performedAttacks.push([x,y]);
         return {x, y};
     }
 
     return {
         ...player,
-        getNextAttack
+        getNextAttack,
+        getPerformedAttacks
     }
 }
 
