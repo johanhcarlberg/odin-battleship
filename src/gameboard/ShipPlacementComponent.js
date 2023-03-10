8f44c977ac8f324bae84c6fb978ce7a9fa827f6f
JSHC <johan.hansson.carlberg@gmail.com>
2023-03-10 10:45:15 +0100

    Add drag event listener, move DOM update logic to separate function

--- src/gameboard/ShipPlacementComponent.js
+++ src/gameboard/ShipPlacementComponent.js
@@ -1,22 +1,35 @@
 import './ShipPlacementComponent.css';
+import PubSub from 'pubsub-js';
 
 export class ShipPlacementComponent {
     constructor(gameboard) {
         this.gameboard = gameboard;
+        PubSub.subscribe('shipsPlacedChanged', () => this.updateShipsToPlace());
     }
 
     render() {
         this.shipPlacementContainer = document.createElement('div');
         this.shipPlacementContainer.classList.add('ship-placement-container');
 
-/*         if (this.gameboard.getShipsToPlace().length === 0) {
+        this.updateShipsToPlace();
+
+        return this.shipPlacementContainer;
+    }
+
+    updateShipsToPlace() {
+        if (this.gameboard.getShipsToPlace().length === 0) {
             this.shipPlacementContainer.classList.add('hidden');
-        } */
+            return;
+        } 
+        this.shipPlacementContainer.innerHTML = '';
 
         for (const shipToPlace of this.gameboard.getShipsToPlace()) {
             const shipDiv = document.createElement('div');
             shipDiv.classList.add('ship-to-place');
             shipDiv.draggable = true;
+            shipDiv.addEventListener('dragstart', (e) => {
+                e.dataTransfer.setData("text/plain", shipToPlace);
+            })
             for (let i = 0; i < shipToPlace; i++) {
                 const shipCell = document.createElement('div');
                 shipCell.classList.add('ship-to-place-cell');
@@ -25,7 +38,5 @@ export class ShipPlacementComponent {
 
             this.shipPlacementContainer.appendChild(shipDiv);
         }
-
-        return this.shipPlacementContainer;
     }
 }
